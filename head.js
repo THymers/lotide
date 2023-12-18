9b40041e28747e29ac2d0a88a4bb39eb2618d042
Tara Hymers <hymerstara@gmail.com>
2023-12-18 01:56:25 +0000

    updated AI suggestion

--- head.js
+++ head.js
@@ -6,11 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 const head = function(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (i === 0) {
-      return array[i];
-    }
-  }
+  return array[0];
 };
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

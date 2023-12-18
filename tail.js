5e33ec9555d9e4985f36d3df5e799ad8a1e30778
Tara Hymers <hymerstara@gmail.com>
2023-12-18 05:30:45 +0000

    create while saved

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const tail = function(notHead) {
+  return notHead.slice[0];
+
+};

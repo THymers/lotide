5d993ac259b8cea9aa36f86b0ff5d9e1db0c7aca
Tara <hymerstara@gmail.com>
2024-01-05 20:04:15 -0700

    added ticks

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,8 +18,8 @@ const eqArrays = function (array1, array2) {
 };
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(✅✅✅ Assertion Passed: ${array1} === ${array2}); 
-     console.log(🛑🛑🛑 Assertion Failed: ${array1} !== ${array2});
+    console.log('✅✅✅ Assertion Passed: ${array1} === ${array2}'); 
+     console.log('🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}');
   }
 };
 

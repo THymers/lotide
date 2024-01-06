2ab4ad17a5b61e24d855dd09cc503e3bf2f8cda0
Tara <hymerstara@gmail.com>
2024-01-05 21:36:39 -0700

    didnt work

--- middle.js
+++ middle.js
@@ -25,11 +25,11 @@ const assertArraysEqual = function (array1, array2) {
 };
 const middle = function (array) {
   if (array.length <= 2) {
-    return [];
+    return ([]);
   } else if (array.length % 2 !== 0) {
-    return [array[Math.floor(array.length / 2)]];
+    return ([array[Math.floor(array.length / 2)]]);
   } else {
-    return [array[array.length / 2 - 1], array[array.length / 2]];
+    return ([array[array.length / 2 - 1], array[array.length / 2]]);
   }
 };
 

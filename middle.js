2f2bf4e6f8270e8e4d0bd7f76749a8cffd6c1e14
Tara <hymerstara@gmail.com>
2024-01-05 21:29:46 -0700

    middle function

---
+++ middle.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+const middle = function (array) {
+  if (array.length <= 2) {
+    console.log([]);
+  } else if (array.length % 2 !== 0) {
+    console.log([array[Math.floor(array.length / 2)]]);
+  } else {
+    console.log([array[array.length / 2 - 1], array[array.length / 2]]);
+  }
+};
+
+middle([1]); // => []
+middle([1, 2]); // => []
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]

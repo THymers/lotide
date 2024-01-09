8122963fd1ebc0c3a2040587b5001b3f79e8e6c3
Tara <hymerstara@gmail.com>
2024-01-08 22:12:11 -0700

    found key

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  mystery: "The Mentalist",
+  talk_show: "Oprah",
+  late_night: "jimmy Kimmel"
+};
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+

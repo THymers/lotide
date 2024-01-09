75df7f538eeaaa1675de6c16a8d3769ef42f6778
Tara <hymerstara@gmail.com>
2024-01-08 18:25:48 -0700

    AI recos

--- countOnly.js
+++ countOnly.js
@@ -9,14 +9,11 @@ const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
-    {
-      if (itemsToCount[item]) {
-        if (results[item]) {
-          results[item] += 1;
-        } else {
-          results[item] = 1;
-        }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
     }
   }

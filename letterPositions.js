05d02cfaf072ee3f4c37b5b668ac6462d31e06d2
Tara <hymerstara@gmail.com>
2024-01-08 19:58:37 -0700

    trying things

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i].toLowerCase();
+    if (letter >= "a" && letter <= "z") {
+      // Check if it's a letter
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+console.log(letterPositions("lighthouse in the house"));

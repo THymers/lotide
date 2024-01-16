7941279ef78086d581e1df9f01e62aee7154ad2c
Tara <hymerstara@gmail.com>
2024-01-16 15:51:31 -0700

    modified tests

--- takeUntil.js
+++ takeUntil.js
@@ -41,3 +41,7 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+//tests
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); 
+assertArraysEqual(takeUntil(data2, x => x < 0), [5, 6, 12, 17, 22]); 

a5590b044bc0c46333ae464b0bda95e0c7932b28
Tara <hymerstara@gmail.com>
2024-01-16 16:58:24 -0700

    added mod.ex and removed tests

--- head.js
+++ head.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;

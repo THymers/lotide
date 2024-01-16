4bfb3eb1721d056b22542296df7f610f997ab899
Tara <hymerstara@gmail.com>
2024-01-16 16:22:31 -0700

    tried new

--- findKey.js
+++ findKey.js
@@ -1,29 +1,31 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
   } else {
     console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
-const findKey = function(obj, predicate) {
+const findKey = function (obj, callback) {
   for (const key in obj) {
     if (Object.hasOwnProperty.call(obj, key)) {
-      if (predicate(obj[key])) {
+      if (callback(obj[key])) {
         return key;
       }
     }
   }
-  return undefined; 
-}
-const result = findKey({
-  "Best Friends": { stars: 1 },
-  "Good Times":   { stars: 3 },
-  "best":      { stars: 2 },
-  "ouBletr":   { stars: 3 },
-  "wOw":       { stars: 2 },
-  "Awesome":  { stars: 3 }
-}, x => x.stars === 2) 
-
+  return undefined;
+};
+const result = findKey(
+  {
+    "Best Friends": { stars: 1 },
+    "Good Times": { stars: 3 },
+    best: { stars: 2 },
+    ouBletr: { stars: 3 },
+    wOw: { stars: 2 },
+    Awesome: { stars: 3 },
+  },
+  (x) => x.stars === 2,
+);
 
 //tests
 assertEqual(result, "best");

1e9aa6c93239d8d6a49dbd2502c7a674e3efe824
Tara <hymerstara@gmail.com>
2024-01-16 17:30:59 -0700

    added mod.ex

--- tail.js
+++ tail.js
@@ -1,22 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const tail = function(notHead) {
   return notHead.slice(1);
-
 };
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
+module.exports = tail;

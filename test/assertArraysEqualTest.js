c87187001a07793786a9ec3627c2771b42f05f00
Tara <hymerstara@gmail.com>
2024-01-19 23:03:43 -0700

    added tests

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,2 +1,10 @@
 const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+
+
+//tests
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+const arr3 = [1, 2, 4];
+
+assertArraysEqual(arr1, arr2); 
+assertArraysEqual(arr1, arr3);

8c14bf41426dcd8fea8766468b2ff6f027ac3f32
Tara <hymerstara@gmail.com>
2024-01-19 22:41:42 -0700

    test created

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertEqual(eqArrays([1,2,3], [1,2]), false);
+
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays(["test", "123"], ["test", "123"]), true);
+assertEqual(eqArrays(["test", "123"], ["tes", "123"]), false);

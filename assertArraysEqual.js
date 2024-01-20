a8b8ffd92a211afbf4bf496573db90fd05746b78
Tara <hymerstara@gmail.com>
2024-01-19 22:51:35 -0700

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); 
@@ -30,3 +14,5 @@ const arr3 = [1, 2, 4];
 
 assertArraysEqual(arr1, arr2); 
 assertArraysEqual(arr1, arr3);
+
+module.exports = assertArraysEqual

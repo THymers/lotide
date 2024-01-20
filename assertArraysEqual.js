235647c878a2cb38b06f2fbc0321294e7e5cd44b
Tara <hymerstara@gmail.com>
2024-01-19 23:02:52 -0700

    extra code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,11 +8,4 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-const arr1 = [1, 2, 3];
-const arr2 = [1, 2, 3];
-const arr3 = [1, 2, 4];
-
-assertArraysEqual(arr1, arr2); 
-assertArraysEqual(arr1, arr3);
-
 module.exports = assertArraysEqual

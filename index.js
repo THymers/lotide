7952ae9c46ddca3cb4f0c389daaf94203aab4c28
Tara <hymerstara@gmail.com>
2024-01-20 00:45:42 -0700

    missing functions

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const eqArrays = require('.eqArrays');
+const assertEqual = require('.assertEqual');
+const assertArraysEqual = require('.assertArraysEqual');
+const assertObjectsEqual = require('.assertObjectsEqual');
+const countLetters = require('.countLetters');
+const countOnly = require('.countOnly');
+const eqObjects = require('.eqObjects');
+const findKey = require('.findKey');
+const findKeyByValue = require('.findKeyByValue');
+const flatten = require('.flatten');
+const letterPositions = require('.letterPositions');
+const takeUntil = require('.takeUntil');
+const without = require('.without');
+
+
+
 
 module.exports = {
   head:   head,
@@ -8,5 +24,18 @@ module.exports = {
   middle: middle,
   eqArrays: eqArrays,
   assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
 };
+
+
+

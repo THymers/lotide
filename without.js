0dfc844b1820b707e1ebaa4d7d5a0121bf4b0506
Tara <hymerstara@gmail.com>
2024-02-04 21:37:49 -0700

    review code to submit

--- without.js
+++ without.js
@@ -1,29 +1,8 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); 
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-const without = function(array, remove) {
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
+const without = function (array, remove) {
   const removedItems = [];
   for (let i = 0; i < array.length; i++) {
     if (!remove.includes(array[i])) {
@@ -33,14 +12,4 @@ const without = function(array, remove) {
   return removedItems;
 };
 
-
-console.log(without([1, 2, 3], [1])); 
-console.log(without(["1", "2", "3"], [1, 2, "3"])); 
-
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"])); 
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); 
-assertArraysEqual(without(["1", "2", "3"], [1, 2]), ["1", "2", "3"]);
+module.exports = without;

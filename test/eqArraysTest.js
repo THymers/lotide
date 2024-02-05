2196c98002149855f5c36b863908ff8829c03072
Tara <hymerstara@gmail.com>
2024-02-04 21:18:07 -0700

    review code

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,8 @@
-const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
 
-assertEqual(eqArrays([1,2,3], [1,2,3]), true);
-assertEqual(eqArrays([1,2,3], [1,2]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
 
 assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays(["test", "123"], ["test", "123"]), true);

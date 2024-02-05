666406ed1fa34bb2b6afc0a06e2fda628b85246b
Tara <hymerstara@gmail.com>
2024-02-04 21:37:21 -0700

    review code to submit

--- takeUntil.js
+++ takeUntil.js
@@ -1,47 +1,14 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); 
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-const takeUntil = function(array, callback) {
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
+const takeUntil = function (array, callback) {
   for (let i = 0; i < array.length; i++) {
     if (callback(array[i])) {
       return array.slice(0, i);
     }
   }
-  return array; 
-}
-//expected input: 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+  return array;
+};
 
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-
-//tests
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); 
-assertArraysEqual(takeUntil(data2, x => x === ',' ), [5, 6, 12, 17, 22])
+module.exports = takeUntil;

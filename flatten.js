86425637f6b73bb08a33527f16cde292ae8d1d9f
Tara <hymerstara@gmail.com>
2024-02-04 21:32:44 -0700

    review for submission

--- flatten.js
+++ flatten.js
@@ -1,32 +1,11 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); 
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
+
 const flatten = function (array) {
   let result = [];
   for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) { 
+    if (Array.isArray(array[i])) {
       result = result.concat(array[i]);
     } else {
       result.push(array[i]);
@@ -35,5 +14,4 @@ const flatten = function (array) {
   return result;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flatten;

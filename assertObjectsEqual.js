8a5e8250a76ff86c0f7df42588341777cd1e617d
Tara <hymerstara@gmail.com>
2024-02-04 19:39:10 -0700

    add module.exports

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -36,11 +36,17 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 // FUNCTION IMPLEMENTATION
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
   if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`);
+    console.log(
+      `👍👍 Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`
+    );
   } else {
-    console.log(`👎👎 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`);
+    console.log(
+      `👎👎 Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`
+    );
   }
-}
+};
+
+module.exports = assertObjectsEqual;

68436eed9d94c1609bdd943575b983621eac0046
Tara <hymerstara@gmail.com>
2024-02-04 21:28:07 -0700

    clean up and review code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,11 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`); 
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
-module.exports = assertArraysEqual
+module.exports = assertArraysEqual;

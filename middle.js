51f1fee74c2d5365f0448d200f4f74e09d0db025
Tara <hymerstara@gmail.com>
2024-02-04 21:36:19 -0700

    reformat code

--- middle.js
+++ middle.js
@@ -1,13 +1,11 @@
 const middle = function (array) {
   if (array.length <= 2) {
-    return ([]);
+    return [];
   } else if (array.length % 2 !== 0) {
-    return ([array[Math.floor(array.length / 2)]]);
+    return [array[Math.floor(array.length / 2)]];
   } else {
-    return ([array[array.length / 2 - 1], array[array.length / 2]]);
+    return [array[array.length / 2 - 1], array[array.length / 2]];
   }
 };
 
 module.exports = middle;
-
-

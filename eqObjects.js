232c5d20f60cc163fc228d495e49405b80f1459b
Tara <hymerstara@gmail.com>
2024-02-04 21:31:11 -0700

    submit final code

--- eqObjects.js
+++ eqObjects.js
@@ -1,21 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertEquals = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const eqObjects = function (object1, object2) {
   const shirtOne = Object.keys(object1);
@@ -36,15 +19,4 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
-assertEquals(eqObjects(shirtObject, anotherShirtObject), true);
-
-const longSleeveShirtObject = {
-  size: "medium",
-  color: "red",
-  sleeveLength: "long",
-};
-eqObjects(shirtObject, longSleeveShirtObject); // => false
-assertEquals(eqObjects(shirtObject, longSleeveShirtObject), false);
+module.exports = eqObjects;

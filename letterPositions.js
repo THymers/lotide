404e564db11a2f537ea36fb825b9a53c99cfe380
Tara <hymerstara@gmail.com>
2024-02-04 21:34:53 -0700

    review for submitting

--- letterPositions.js
+++ letterPositions.js
@@ -1,34 +1,12 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`👎👎 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
-    if (letter >= 'a' && letter <= 'z') {
+    if (letter >= "a" && letter <= "z") {
       if (results[letter]) {
         results[letter].push(i);
       } else {
@@ -38,5 +16,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("this is a test").t, [0, 10, 13]);
+
+module.exports = letterPositions;
